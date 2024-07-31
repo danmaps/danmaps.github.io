fc4cdafc8310dd009c90b9a55d98512d626d51cb
Danny McVey <dannybmcvey@gmail.com>
2024-07-31 13:51:30 -0700

    remove freeze stuff and add streamlit

--- app.py
+++ app.py
@@ -17,10 +17,16 @@ def post(post_name):
     try:
         with open(os.path.join(POSTS_DIR, f'{post_name}.md'), 'r') as f:
             content = f.read()
+
         html_content = markdown(content)
-        return render_template('post.html', content=html_content, title=post_name)
+
+        # Check if 'streamlit' is in the post_name to decide whether to embed the app
+        embed_streamlit = 'streamlit' in post_name
+
+        return render_template('post.html', content=html_content, title=post_name, embed_streamlit=embed_streamlit)
     except FileNotFoundError:
         abort(404)
+
 
 if __name__ == '__main__':
     # Get the port from environment variable or default to 5000
